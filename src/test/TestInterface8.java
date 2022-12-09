2b04189e70991e62ff59301cb7058fb21e5340c0
HoangAtCode <hoangat3112@gmail.com>
2022-12-10 00:13:18 +0700

    add interface java 8 test

---
+++ src/test/TestInterface8.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class TestInterface8 {
+    public static void main(String[] args) {
+        Regtangle regtangle = new Regtangle();
+        Printable.saySomething();
+        regtangle.defaultPrint();
+
+        TestFunctionalInterface test = new TestFunctionalInterface();
+        test.myFunctionalInterface.test();
+        test.execute.accept(1);
+        System.out.println(test.data);
+    }
+
+
+
+}
+
+interface Printable{
+    static int data = 5;
+    static void saySomething(){
+        System.out.println("Printable");
+    }
+
+    default void defaultPrint(){
+        System.out.println(data);
+    }
+    void print();
+}
+interface Drawable extends Printable{
+    void draw();
+}
+
+class Regtangle implements  Printable{
+
+    @Override
+    public void print() {
+        System.out.println("reg");
+    }
+}
+
+@FunctionalInterface
+interface MyFunctionalInterface{
+    void test();
+}
+
+class TestFunctionalInterface {
+    public MyFunctionalInterface myFunctionalInterface = new MyFunctionalInterface() {
+        @Override
+        public void test() {
+            data = 11;
+        }
+    };
+
+    public int data = 10;
+
+    public Consumer<Integer> execute = (Integer a)->{
+        int b = 11;
+
+        data = 11;
+    };
+
+
+}
